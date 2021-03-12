929cf06776f49a420508a0304365a4aaaf6bfd41
dcolivier3 <dcolivier3@gmail.com>
2021-03-12 19:22:14 +0200

    added MeshUtils.cs from codemonkey

---
+++ CoffeeShopGit/Coffee Shop Management/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// code monkey grid genration https://www.youtube.com/watch?v=waEsGu--9P8&list=PLzDRvYVwl53uhO8yhqxcyjDImRjO9W722&index=1
+public class Grid 
+{
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+    public Grid(int width, int height){ // basic setup an array that takes two ints
+
+        this.width = width;
+        this.height = height; 
+
+        gridArray = new int[width, height];
+
+        Debug.Log(width + "+" + height); 
+    }
+
+}
---
+++ CoffeeShopGit/Coffee Shop Management/Assets/Scripts/Grid/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// codemonkey Grid Testing system https://www.youtube.com/watch?v=waEsGu--9P8&list=PLzDRvYVwl53uhO8yhqxcyjDImRjO9W722&index=1
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid grid = new Grid(20, 10); // tests grid in Grid.cs
+    }
+
+    
+}
